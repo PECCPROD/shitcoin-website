3d47e15ef080f1eae42b28da4e797af6b371e150
PECCPROD <peruesclavecoin@gmail.com>
2025-02-10 21:45:39 -0500

    Initial commit

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/shitcoin-website/", // Set your repo name here
+base: '/PERUESCLAVECOIN/', // Set your repo name here
 });
